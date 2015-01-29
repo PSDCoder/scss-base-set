88be80cbe0f29fe1d036a024fea5ac4360d3ec35
Pavel Grinchenko <psdcoder@gmail.com>
2015-01-29 23:18:16 +0300

    Changed gulp build flow, replated inlcude-media to new version which now not crash libsass building flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var colors = require('colors/safe');
+
 var sassMainPath = 'src/main.scss';
 var distPath = 'dist/';
 
 gulp
-    .task('default', ['scss', 'watch'])
+    .task('default', ['watch', 'scss'])
     .task('scss', function() {
         gulp.src(sassMainPath)
             .pipe($.plumber())
             .pipe($.sourcemaps.init())
-            .pipe($.sass())
+            .pipe($.sass({
+                outputStyle: 'nested',
+                errLogToConsole: false,
+                onError: function(err) {
+                    return console.log(
+                        colors.red.underline(err.message + "\nin " + err.file + "\non " + err.line + ":" + err.column)
+                    );
+                }
+            }))
             .pipe($.autoprefixer())
             .pipe($.sourcemaps.write('./'))
             .pipe(gulp.dest(distPath));
