56832dbb9e0025eee768bc249a0e44d4d19b006d
Pavel Grinchenko <psdcoder@gmail.com>
2015-02-07 16:00:44 +0300

    Added demo.scss build to gulp config and added livereloading (with chrome plugin)

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,38 @@ var colors = require('colors/safe');
 var sassMainPath = 'src/main.scss';
 var distPath = 'dist/';
 
+var demoScss = 'demo/demo.scss';
+var demoFolder = 'demo/';
+
 gulp
-    .task('default', ['watch', 'scss'])
+    .task('default', ['watch', 'scss', 'demo-scss'])
     .task('scss', function() {
-        gulp.src(sassMainPath)
-            .pipe($.plumber())
-            .pipe($.sourcemaps.init())
-            .pipe($.sass({
-                outputStyle: 'nested',
-                errLogToConsole: false,
-                onError: function(err) {
-                    return console.log(
-                        colors.red.underline(err.message + "\nin " + err.file + "\non " + err.line + ":" + err.column)
-                    );
-                }
-            }))
-            .pipe($.autoprefixer())
-            .pipe($.sourcemaps.write('./'))
-            .pipe(gulp.dest(distPath));
+        sassBuild(sassMainPath, distPath);
+    })
+    .task('demo-scss', function() {
+        sassBuild(demoScss, demoFolder);
     })
     .task('watch', function () {
+        $.livereload.listen();
         gulp.watch('src/**/*.scss', ['scss']);
+        gulp.watch(demoScss, ['demo-scss']);
     });
+
+function sassBuild(sassMainPath, distPath) {
+    gulp.src(sassMainPath)
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
+            outputStyle: 'nested',
+            errLogToConsole: false,
+            onError: function(err) {
+                return console.log(
+                    colors.red.underline(err.message + "\nin " + err.file + "\non " + err.line + ":" + err.column)
+                );
+            }
+        }))
+        .pipe($.autoprefixer())
+        .pipe($.sourcemaps.write('./'))
+        .pipe(gulp.dest(distPath))
+        .pipe($.livereload());
+}
