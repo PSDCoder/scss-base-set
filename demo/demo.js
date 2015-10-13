714d6a12fdfb50338b4c5839e8c7ab85aaeedc95
Pavel Grinchenko <psdcoder@gmail.com>
2015-10-13 21:49:42 +0300

    Updated gulp build config. Started new set structure. Completed first base module: 'Typography'

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,15 @@
+var gridToggler = document.querySelector('.vertical-rhythm-toggler');
+var grid = document.querySelector('.vertical-rhythm-guidelines');
+
+gridToggler.addEventListener('click', function () {
+    gridToggler.classList.toggle('vertical-rhythm-toggler_enabled');
+    gridToggler.classList.toggle('vertical-rhythm-toggler_disabled');
+    grid.classList.toggle('vertical-rhythm-guidelines_enabled');
+    grid.classList.toggle('vertical-rhythm-guidelines_disabled');
+});
+
+
+
 var body = document.body;
 var demoItemsButton = document.querySelector('.demo-items__button');
 var demoItemsList = document.querySelector('.demo-items__list');
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,100 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+        'serve': 's',
+        'watch': 'w',
+        'tunnel': 't'
+    }
+});
 var $ = require('gulp-load-plugins')();
 var colors = require('colors/safe');
+var browserSync = require('browser-sync').create();
+
+var WATCH = !!argv.watch;
+var SERVE = !!argv.serve;
+var TUNNEL = !!argv.tunnel;
 
 var sassMainPath = 'src/main.scss';
-var distPath = 'dist/';
-
+var distPath = 'dist';
 var demoScss = 'demo/demo.scss';
-var demoFolder = 'demo/';
+var demoFolder = 'demo';
 
 gulp
-    .task('default', ['watch', 'scss', 'demo-scss'])
+    .task('default', function (cb) {
+        var tasksList = ['build'];
+
+        if (SERVE) {
+            tasksList.push('serve');
+        }
+
+        if (WATCH) {
+            tasksList.push('watch');
+        }
+
+        tasksList.push(cb);
+
+        runSequence.apply(runSequence, tasksList);
+    })
+    .task('build', function (cb) {
+        runSequence('scss', 'demo-scss', cb);
+    })
     .task('scss', function() {
-        sassBuild(sassMainPath, distPath);
+        return sassBuild(sassMainPath, distPath);
     })
     .task('demo-scss', function() {
-        sassBuild(demoScss, demoFolder);
+        return sassBuild(demoScss, demoFolder);
     })
     .task('watch', function () {
-        $.livereload.listen();
-        gulp.watch('src/**/*.scss', ['scss']);
-        gulp.watch(demoScss, ['demo-scss']);
+        $.watch('src/**/*.scss', function () { runSequence('scss'); })
+            .on('error', errorHandler);
 
-        gulp.watch('demo/index.html')
-            .on('change', function (e) {
-                $.livereload.changed(e.path);
-            });
+        $.watch(demoScss, function () { runSequence('demo-scss'); })
+            .on('error', errorHandler);
+
+        if (SERVE) {
+            $.watch(['demo/*.*', 'dist/*.*'])
+                .on('change', browserSync.reload)
+                .on('error', errorHandler)
+        }
+    })
+    .task('serve', function(cb) {
+        browserSync.init({
+            server: {
+                baseDir: '.'
+            },
+            tunnel: TUNNEL,
+            startPath: 'demo/index.html'
+        }, cb);
     });
 
+
 function sassBuild(sassMainPath, distPath) {
-    gulp.src(sassMainPath)
+    var stream = gulp.src(sassMainPath)
         .pipe($.plumber())
         .pipe($.sourcemaps.init())
         .pipe($.sass({
-            outputStyle: 'nested',
-            errLogToConsole: false,
-            onError: function(err) {
-                return console.log(
-                    colors.red.underline(err.message + "\nin " + err.file + "\non " + err.line + ":" + err.column)
-                );
-            }
-        }))
+            outputStyle: 'nested'
+        }).on('error', sassErrorHandler))
+
         .pipe($.autoprefixer())
         .pipe($.sourcemaps.write('./'))
-        .pipe(gulp.dest(distPath))
-        .pipe($.livereload());
+        .pipe(gulp.dest(distPath));
+
+    if (SERVE) {
+        stream = stream.pipe(browserSync.stream());
+    }
+
+    return stream;
+}
+
+function sassErrorHandler(error) {
+    error.messageFormatted = colors.red(error.messageFormatted);
+    return $.sass.logError.call(this, error);
+}
+
+function errorHandler(error) {
+    return console.log(colors.red(
+        error.message + "\nin " + error.file + "\non " + error.line + ":" + error.column
+    ));
 }
